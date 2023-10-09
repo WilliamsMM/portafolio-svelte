f867d9e6f8e926e126f7b69c2f0644c608d79116
Erick Ramírez <erick.ramirez@linktic.com>
2023-10-09 01:09:51 -0400

    Adding new styles and logic for navigations

---
+++ src/lib/components/BoxAnimated.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let ms = 200
+	let isActive = false
+
+	setTimeout(() => {
+		isActive = true
+	}, ms)
+</script>
+
+<div class={`transition-all duration-[${ms}] ${!isActive ? 'opacity-0 scale-50' : null}`}>
+	<slot />
+</div>
--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -7,45 +7,54 @@
 	import SVELTEKIT from '$lib/assets/sveltekit.png'
 	import { fly } from 'svelte/transition'
 	import { animationConfig } from '$lib/utils/animation-config'
+	import BoxAnimated from '$lib/components/BoxAnimated.svelte'
 
 	// Habilidades
 	const skills = [
 		{
 			name: 'HTML 5',
-			img: HTML
+			img: HTML,
+			ms: 200,
 		},
 		{
 			name: 'CSS',
-			img: CSS
+			img: CSS,
+			ms: 300,
 		},
 		{
 			name: 'JavaScript',
-			img: JS
+			img: JS,
+			ms: 400,
 		},
 		{
 			name: 'TypeScript',
-			img: TS
+			img: TS,
+			ms: 500,
 		},
 		{
 			name: 'SvelteKit',
-			img: SVELTEKIT
+			img: SVELTEKIT,
+			ms: 600,
 		},
 		{
 			name: 'Github',
-			img: GITHUB
+			img: GITHUB,
+			ms: 700,
 		}
 	]
 </script>
 
 <div class="flex max-w-4xl mx-auto flex-wrap justify-center items-center gap-10">
 	{#each skills as skill}
-		<div
-			class="w-40 p-4 flex flex-col gap-2 items-center justify-center bg-white bg-opacity-20 rounded-lg shadow-2xl"
-		>
-			<img class="h-28 object-cover rounded-xl" src={skill.img} alt={skill.name} />
-			<h3 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-				{skill.name}
-			</h3>
-		</div>
+		<BoxAnimated ms={skill.ms}>
+			<div
+				class="w-40 p-4 flex flex-col gap-2 items-center justify-center bg-white bg-opacity-20 rounded-lg shadow-2xl"
+			>
+				<img class="h-28 object-cover rounded-xl" src={skill.img} alt={skill.name} />
+				<h3 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+					{skill.name}
+				</h3>
+			</div>
+		</BoxAnimated>
 	{/each}
 </div>
